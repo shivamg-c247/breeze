aeeac124f280d2986faf10ab728e8db2e7ab4e3e
shivamgchapter247 <shivam.g@chapter247.com>
2021-05-13 17:47:32 +0530

    Laravel 8 Breeze AUTH

---
+++ resources/views/user/show.blade.php
@@ -0,0 +1,33 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+                        <!-- Name -->
+                        <div>
+                            <x-label for="name" :value="__('Name')" />
+                            <x-input id="name" class="block mt-1 w-full" type="text" name="name" value="{{$data['name']}}" required autofocus readonly />
+                        </div>
+                        <!-- Email Address -->
+                        <div class="mt-4">
+                            <x-label for="email" :value="__('Email')" />
+                            <x-input id="email" class="block mt-1 w-full" type="email" name="email" value="{{$data['email']}}" required readonly />
+                        </div>
+                        <!-- <div class="flex items-center justify-end mt-4">
+                            <x-button class="ml-4">
+                                {{ __('Save') }}
+                            </x-button>
+                        </div> -->
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
